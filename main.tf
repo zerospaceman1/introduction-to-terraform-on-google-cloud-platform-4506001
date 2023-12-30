161ec9d88e3a186cb02928fa42220886f057a569
Josh Samuelson <josh@joshsamuelson.com>
2023-12-30 12:23:50 -0800

    qa staging and prod

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ module "qa" {
   app_name     = "qa-blog"
   network_name = "qa"
 }
+
+module "staging" {
+  source = "./modules/blog"
+
+  project_id = var.project_id
+  region     = var.region
+
+  app_name     = "staging-blog"
+  network_name = "staging"
+}
+
+module "prod" {
+  source = "./modules/blog"
+
+  project_id = var.project_id
+  region     = var.region
+
+  app_name     = "prod-blog"
+  network_name = "prod"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "qa_ip" {
   value = module.qa.public_ip
 }
+
+output "staging_ip" {
+  value = module.staging.public_ip
+}
+
+output "prod_ip" {
+  value = module.prod.public_ip
+}
