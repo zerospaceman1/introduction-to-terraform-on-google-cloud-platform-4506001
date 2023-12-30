4dde13566f9cb17f8285468c024f9665213850d6
Josh Samuelson <josh@joshsamuelson.com>
2023-12-30 12:03:44 -0800

    Output Blog

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,2 @@
 output "public_ip" {
-  value = google_compute_instance.web.network_interface[0].access_config.0.nat_ip
-}
+  value = google_compute_instance.blog.network_interface[0].access_config.0.nat_ip
