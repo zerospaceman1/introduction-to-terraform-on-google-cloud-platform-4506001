f6a50b9f392bbc45a639ccbf698b2b6229974a7c
Josh Samuelson <josh@joshsamuelson.com>
2023-12-30 11:59:23 -0800

    Network

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+resource "google_compute_network" "app" {
+  name                    = "app"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "app" {
+  name          = "app"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-west1"
+  network       = google_compute_network.app.id
+}
+
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = "ubuntu-os-cloud" 
@@ -8,14 +20,13 @@ resource "google_compute_instance" "web" {
   name         = "web"
   machine_type = "e2-micro"
 
-  
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
     }
   }
   network_interface {
-   subnetwork = "default"
+   subnetwork = "app"
    access_config {
       # Leave empty for dynamic public IP
     }
