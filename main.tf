67f54e7cafda30a4728bc5b8dd45f3ac157a5f08
Josh Samuelson <josh@joshsamuelson.com>
2023-12-30 11:54:08 -0800

    Starter Code

---
+++ main.tf
@@ -0,0 +1,24 @@
+data "google_compute_image" "ubuntu" {
+  most_recent = true
+  project     = "ubuntu-os-cloud" 
+  family      = "ubuntu-2204-lts"
+}
+
+resource "google_compute_instance" "web" {
+  name         = "web"
+  machine_type = "e2-micro"
+
+  
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.ubuntu.self_link
+    }
+  }
+  network_interface {
+   subnetwork = "default"
+   access_config {
+      # Leave empty for dynamic public IP
+    }
+  }  
+
+}
---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = ""
+  region  = "us-west1"
+  zone    = "us-west1-a"
+}
