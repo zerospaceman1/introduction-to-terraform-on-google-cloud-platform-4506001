0dd77ef1cb3ee581c919fa39d2f4d3ec0a74d758
zerospaceman1 <ryne.zalinko@gmail.com>
2025-02-03 16:23:57 -0800

    blog

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ data "google_compute_image" "ubuntu" {
   family      = "ubuntu-2204-lts"
 }
 
-resource "google_compute_instance" "web" {
-  name         = "web"
+resource "google_compute_instance" "blog" {
+  name         = "blog"
   machine_type = "e2-micro"
 
   
